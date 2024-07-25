ae6d7516504d84d5060bf7dac409692586226b75
Roger <ronguerra51@gmail.com>
2024-07-25 10:26:46 -0600

    se corrigio el href en el navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -9,10 +9,10 @@
             </a>
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="../views/empleados/index.php">EMPLEADOS</a>
+                    <a class="nav-link active" aria-current="page" href="/tarea3_carpio_guerra_is2/views/empleados/index.php">EMPLEADOS</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="../views/puestos/index.php">PUESTOS</a>
+                    <a class="nav-link active" aria-current="page" href="/tarea3_carpio_guerra_is2/views/puestos/index.php">PUESTOS</a>
                 </li>
             </ul>
         </div>
